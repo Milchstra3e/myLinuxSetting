851dcaa8b8f1b08ea4ee695f6bf6be390b4a94f1
Milchstra3e <milchstra3e@gmail.com>
2018-06-28 19:04:00 +0800

    Update install_2.sh

--- install_2.sh
+++ install_2.sh
@@ -1,10 +1,12 @@
 # install oh-my-zsh
+sudo curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh
+
+# install powerline
 sudo apt-get install python-pip git
 sudo easy_install --upgrade pip
 sudo apt-get install git+git://github.com/Lokaltog/powerline
 
-# install powerline
-wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
+sudo wget https://github.com/Lokaltog/powerline/raw/develop/font/PowerlineSymbols.otf https://github.com/Lokaltog/powerline/raw/develop/font/10-powerline-symbols.conf
 sudo mv PowerlineSymbols.otf /usr/share/fonts/
 sudo fc-cache -vf
 sudo mv 10-powerline-symbols.conf /etc/fonts/conf.d/
